7a8f8d3b67fe6c2110110e96df0ada0cce172352
Shreyas Joshi <helloshreyasj@gmail.com>
2024-05-29 14:17:17 +0400

    personal information done

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,32 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        
+        while(true){
+            System.out.print("First Name: ");
+            String firstName = scanner.nextLine();
+            
+            //the code will break if firstName is empty
+            
+            if(firstName.isEmpty()){
+                break;
+            } else {
+                System.out.print("Last Name: ");
+                String lastName = scanner.nextLine();
+                
+                System.out.print("Identification Number: ");
+                String idNum = scanner.nextLine();
+                
+                //to add them to infoCollection
+                
+                infoCollection.add(new PersonalInformation(firstName,lastName,idNum));
+            }
+        }
+        // to get their first and last names individually printed
+        System.out.println();
+        for(PersonalInformation info: infoCollection){
+            String FirstandLast = info.getFirstName() + " " + info.getLastName();
+            System.out.println(FirstandLast);
+        }
     }
 }
