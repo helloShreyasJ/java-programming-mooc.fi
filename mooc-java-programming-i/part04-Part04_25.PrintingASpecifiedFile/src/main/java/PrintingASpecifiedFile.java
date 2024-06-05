53d36d473a0ee0260b5a36adf3c5bea02338ea4f
Shreyas Joshi <helloshreyasj@gmail.com>
2024-06-05 12:00:16 +0400

    did printingaspecifiedfile

--- mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,20 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String fileName;
+        
+        fileName = scanner.nextLine();
+        findFile(fileName);
+    }
+    public static void findFile(String fileName){
+        try(Scanner scanner = new Scanner(Paths.get(fileName))){
+            while(scanner.hasNextLine()){
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e);
+        }
     }
 }
