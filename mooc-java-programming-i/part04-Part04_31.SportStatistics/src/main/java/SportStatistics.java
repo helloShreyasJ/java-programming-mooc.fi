8ba406b14eb253126bd61e538a6928617e055056
Shreyas Joshi <helloshreyasj@gmail.com>
2024-06-09 16:41:34 +0400

    did half of sportstatistics

--- mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -4,10 +4,44 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SportStatistics {
-
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-    }
-
+        System.out.println("File: ");
+        String file = scan.nextLine();
+        System.out.println("Team: ");
+        String teamName = scan.nextLine();
+        
+        ArrayList <Team> records = readRecords(file);
+        int count = 0;
+        for(Team team:records){
+            if(team.getHomeTeam().contains(teamName) || team.getVisitingTeam().contains(teamName)){
+                count++;
+            }
+        }
+        
+        System.out.println("Games: " + count);
+    }   
+    public static ArrayList <Team> readRecords (String file){
+        ArrayList <Team> teams = new ArrayList<>();
+        try(Scanner readFile = new Scanner(Paths.get(file))){
+            String line;
+            String parts[];
+            
+            while(readFile.hasNextLine()){
+                line = readFile.nextLine();
+                parts = line.split(",");
+                
+                String ht = parts[0];
+                String vt = parts[1];
+                int pointsht = Integer.valueOf(parts[2]);
+                int pointsvt = Integer.valueOf(parts[3]);
+                
+                teams.add(new Team(ht,vt,pointsht,pointsvt));
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        return teams;
+    } 
 }
---
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/Team.java
@@ -0,0 +1,33 @@
+
+/**
+ *
+ * @author shreyas
+ */
+public class Team {
+    private String ht;
+    private String vt;
+    private int pointsht;
+    private int pointsvt;
+    private int wins;
+    private int losses;
+    
+    public Team(String homeTeam, String visitingTeam,int pointsHomeTeam, int pointsVisitingTeam){
+        ht = homeTeam;
+        vt = visitingTeam;
+        pointsht = pointsHomeTeam;
+        pointsvt = pointsVisitingTeam;
+    }
+    public String getHomeTeam(){
+        return ht;
+    }
+    public String getVisitingTeam(){
+        return vt;
+    }
+    public int getPointsht(){
+        return pointsht;
+    }
+    public int getPointsvt(){
+        return pointsvt;
+    }
+}
+
