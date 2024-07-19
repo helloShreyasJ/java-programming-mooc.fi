bfd9f404c88c55b9fac15ae10624c9bfe2c57482
Shreyas Joshi <helloshreyasj@gmail.com>
2024-07-19 20:31:12 +0400

    completed identicaltwins

--- mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ mooc-java-programming-i/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -16,4 +16,28 @@ public class Person {
     }
 
     // implement an equals method here for checking the equality of objects
+    // aight
+    
+    public boolean equals(Object compared){
+        //variables located in the same position are equal
+        if(this == compared){
+            return true;
+        }
+        //if the type compared is not Person, then the objects are not equal
+        if(!(compared instanceof Person)) {
+            return false;
+        }
+        //now, to convert the object type compared object into a Person object
+        Person comparedPerson = (Person) compared;
+        
+        //if the values of the objects variables are equal then the objects are equal
+        if (this.name.equals(comparedPerson.name) &&
+            this.weight == comparedPerson.weight &&
+            this.height == comparedPerson.height &&
+            this.birthday.getDay() == comparedPerson.birthday.getDay()&&
+            this.birthday.getMonth() == comparedPerson.birthday.getMonth()&&
+            this.birthday.getYear() == comparedPerson.birthday.getYear()){
+            return true;
+        } return false;
+    }
 }
